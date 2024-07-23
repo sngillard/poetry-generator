3e4593d6f6fa131a7e556d6b8706e9d93131ebf5
Sarah Gillard <108188646+sngillard@users.noreply.github.com>
2024-07-23 12:22:16 -0400

    Removed console.log.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function displayPoem(response) {
-  console.log("poem generated");
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -21,10 +20,6 @@ function generatePoem(event) {
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
   poemElement.innerHTML = `<div class="blink">Generating a poem about ${userPromptInput.value} ✍🏽</div>`;
-
-  console.log("Generating poem");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
 
   axios.get(apiURL).then(displayPoem);
 }
