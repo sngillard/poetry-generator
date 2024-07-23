c7f6d1bb3ec66c8d1b9354fbf2d9df958fe5cbd4
Sarah Gillard <108188646+sngillard@users.noreply.github.com>
2024-07-23 12:20:40 -0400

    Improved user experience of poem generation.

--- src/index.js
+++ src/index.js
@@ -15,8 +15,12 @@ function generatePoem(event) {
   let apiKey = "8t518306o3b66f49626bf9e2c29fafe7";
   let prompt = `User prompt input: Generate a New England poem about ${userPromptInput.value}`;
   let context =
-    "You love to write short poems in the style of class New England poets like Henry David Thoreau, Robert Frost, Louisa May Alcott, Ralph Waldo Emerson, Edgar Allan Poe, Emily Dickinson, and Walt Whitman. Your task is to generate a 4-6 line poem in basic HTML and separate each line with a <br />. Include the user input as the poem title in strong with the first letter uppercase. Be sure to follow the user prompt input.";
+    "You love to write short poems in the style of classic New England poets like Henry David Thoreau, Robert Frost, Louisa May Alcott, Ralph Waldo Emerson, Edgar Allan Poe, Emily Dickinson, and Walt Whitman. Your task is to generate a 4-8 line poem in basic HTML and separate each line with a <br />. Include the user input as the poem title in strong with the first letter uppercase. Be sure to follow the user prompt input.";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="blink">Generating a poem about ${userPromptInput.value} ✍🏽</div>`;
 
   console.log("Generating poem");
   console.log(`Prompt: ${prompt}`);
